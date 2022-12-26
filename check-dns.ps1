8da035903a9e4b4d0930fe340855fd1f8bf1ffd6
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2022-12-26 23:44:28 +0100

    Add files via upload

---
+++ check-dns.ps1
@@ -0,0 +1,15 @@
+﻿Write-Progress "(1/2) Loading Data/frequent-domains.csv..."
+$Table = Import-CSV "$PSScriptRoot/../Data/frequent-domains.csv"
+$NumRows = $Table.Length
+
+Write-Progress "(2/2) Resolving $NumRows domains..."
+$StopWatch = [system.diagnostics.stopwatch]::startNew()
+if ($IsLinux) {
+	foreach($Row in $Table){$nop=dig $Row.Domain +short}
+} else {
+	foreach($Row in $Table){$nop=Resolve-DNSName $Row.Domain}
+}
+[float]$Elapsed = $StopWatch.Elapsed.TotalSeconds
+
+$Average = [math]::round($NumRows / $Elapsed, 1)
+"DNS resolves $Average domains per second"
