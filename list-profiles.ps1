30fe56d8e29369d691edf2c38ba427b4f1c82710
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2022-12-27 17:16:08 +0100

    Add files via upload

---
+++ list-profiles.ps1
@@ -0,0 +1,10 @@
+﻿function ListProfile { param([int]$Level, [string]$Profile, [string]$Location)
+	if (test-path "$Location") { $Existent = "yes" } else { $Existent = "no" }
+	New-Object PSObject -Property @{ 'Level'="$Level"; 'Profile'="$Profile"; 'Location'="$Location"; 'Existent'="$Existent"	}
+}
+function ListProfiles { 
+	ListProfile 1 "AllUsersAllHosts"       $PROFILE.AllUsersAllHosts
+	ListProfile 2 "AllUsersCurrentHost"    $PROFILE.AllUsersCurrentHost
+	ListProfile 3 "CurrentUserAllHosts"    $PROFILE.CurrentUserAllHosts
+	ListProfile 4 "CurrentUserCurrentHost" $PROFILE.CurrentUserCurrentHost
+}
