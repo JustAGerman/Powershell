28e8c6ad83ea6b3c4674b94244c92bf9c5141bac
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2023-01-02 10:43:30 +0100

    Add files via upload

---
+++ write-marquee.ps1
@@ -0,0 +1,20 @@
+﻿param([string]$Text = "Here is some Text that will come from the side, its supposed to  be fancy but I hate it personally", [int]$Speed = 60) # 60 ms pause
+
+function StartMarquee { param([string]$Line)
+	"╔══════════════════════════════════════════════════════════════════════════════════╗"
+	"║                                                                                  ║"
+	"╚══════════════════════════════════════════════════════════════════════════════════╝"
+	$LinePos = $HOST.UI.RawUI.CursorPosition
+	$LinePos.X = 2
+	$LinePos.Y -= 2
+	foreach($Pos in 1 .. $($Line.Length - 80)) {
+		$HOST.UI.RawUI.CursorPosition = $LinePos
+		Write-Host -noNewLine "$($Line.Substring($Pos,80))"
+		Start-Sleep -milliseconds $Speed
+	}
+	" "
+	" "
+	" "
+}
+
+StartMarquee "                                                                                    +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++ $Text +++                                                                                         "
