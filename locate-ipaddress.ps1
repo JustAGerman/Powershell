d0eef3eaea883c681b48e0a02883defdb21d01db
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2023-01-02 10:30:13 +0100

    Add files via upload

---
+++ locate-ipaddress.ps1
@@ -0,0 +1,6 @@
+﻿param([string]$IPaddress= "")
+
+if ($IPaddress -eq "" ) { $IPaddress = read-host "Enter IP address to locate (IPv6 is used in todays age)" }
+
+$result = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPaddress"
+write-output $result
