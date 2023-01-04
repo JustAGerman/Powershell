00e678b49ba4642206a346a8150e4082b1959f55
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2023-01-04 19:45:11 +0100

    Add files via upload

---
+++ list-installed-software.ps1
@@ -0,0 +1,1 @@
+﻿Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*, HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | select-object DisplayName,DisplayVersion,InstallDate | Format-Table -autoSize
