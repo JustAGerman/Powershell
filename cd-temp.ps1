29cba378d97dfcb73b69174e730f19e726caae0e
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2023-01-08 12:08:36 +0100

    Update cd-temp.ps1

--- cd-temp.ps1
+++ cd-temp.ps1
@@ -1,2 +1,11 @@
-﻿$Path = GetTempDir
+function GetTempDir {
+    if ("$env:TEMP" -ne "") { return "$env:TEMP" }
+    if ("$env:TMP" -ne "")  { return "$env:TMP" }
+    if ($IsLinux) { return "/tmp" }
+    return "C:\Temp"
+}
+
+$Path = GetTempDir
+Set-Location "$Path"
+"$Path"
 start-process explorer.exe "$Path"
