5bb5955e573cdea92f121deab2732cbb66256e88
JustAGerman <117590150+JustAGerman@users.noreply.github.com>
2024-02-05 15:38:37 +0100

    Update if.ps1

--- if.ps1
+++ if.ps1
@@ -1,16 +1,27 @@
+# Check if running with administrator privileges
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    # Relaunch the script with administrative privileges
+    Write-Host "Script requires administrator privileges. Restarting with elevated permissions..."
     Start-Process powershell.exe -ArgumentList " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
 }
-# Define the URL to the hosts file
-$githubRawUrl = "https://raw.githubusercontent.com/JustAGerman/Powershell/main/hosts"
 
-# Define the destination path (system's hosts file location)
-$destinationPath = "C:\Windows\System32\drivers\etc\hosts"
+try {
+    # Define the URL to the hosts file
+    $githubRawUrl = "https://raw.githubusercontent.com/JustAGerman/Powershell/main/hosts"
 
-# Download the raw hosts file
-Invoke-WebRequest -Uri $githubRawUrl -OutFile $destinationPath -UseBasicParsing
+    # Define the destination path (system's hosts file location)
+    $destinationPath = "C:\Windows\System32\drivers\etc\hosts"
 
-# flush the DNS cache to apply changes immediately
-ipconfig /flushdns
+    # Download the raw hosts file
+    Write-Host "Downloading hosts file from $githubRawUrl..."
+    Invoke-WebRequest -Uri $githubRawUrl -OutFile $destinationPath -UseBasicParsing
+    Write-Host "Hosts file downloaded successfully."
+
+    # flush the DNS cache to apply changes immediately
+    Write-Host "Flushing DNS cache..."
+    ipconfig /flushdns
+    Write-Host "DNS cache flushed. Script execution completed."
+}
+catch {
+    Write-Host "Error: $_"
+}
